a509bfe16ff68f258dfa2639fd3d1119fd0f04c5
VitorAu <vitoraugustosantiago@outlook.com.br>
2026-05-21 11:03:32 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# wezterm
+# WezTerm Config
+
+Simple WezTerm setup
+
+## 📦 Requirements
+
+- WezTerm
+- JetBrainsMono Nerd Font Mono
+
+---
+
+## 📁 Installation
+
+```bash
+~/.config/wezterm
+```
+
+Config file:
+
+```bash
+wezterm.lua
+```
---
+++ wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
+config.font_size = 14
+
+config.enable_tab_bar = false -- Disable wezterm tab bar
+config.window_decorations = "RESIZE" -- Disables window title bar
+config.color_scheme = "carbonfox" -- Sets the tty color scheme
+
+return config
