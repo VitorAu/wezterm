963f16274f0a2b44a0b25b62332a47862bd97f2e
VitorAu <vitoraugustosantiago@outlook.com.br>
2026-05-31 19:37:50 -0300

    refactor

--- wezterm.lua
+++ wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
-config.font_size = 14
+config.font_size = 15
 
 config.enable_tab_bar = false -- Disable wezterm tab bar
 config.window_decorations = "RESIZE" -- Disables window title bar
